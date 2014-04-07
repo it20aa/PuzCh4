2b158dbb663a41b6d99eb0a52207f58949cfa832
Ilene TEnney <it20a@yahoo.com>
2014-04-07 10:21:06 -0600

    PuzCht
    Pool Puz from Chapter 4

---
+++ Puzze4.java
@@ -0,0 +1,28 @@
+
+class Puzze4 {
+public static void main (String [] args ) {
+	//Something
+	
+	int y =1;
+	int x = 0;
+	int result = 0;
+	while (x < 6) {
+		//somthing
+		y = y*10;
+		// somthing
+	}
+	
+	x=6;
+	while (x > 0) {
+		
+		
+		result = result + 3;//something
+	}
+
+System.out.println("result " + result);
+
+}
+}
+
+
+Class    ()
